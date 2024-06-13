20965a24d9f47bc8d5faf46154a1a3bc2eabc681
ProjectEli <projectschnee@gmail.com>
2024-06-13 15:23:01 +0900

    초기 커밋.

---
+++ TW_Maps_dotnet_framework/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TW_Maps_dotnet_framework
+{
+    public partial class TW_maps : Form
+    {
+        public TW_maps()
+        {
+            InitializeComponent();
+        }
+    }
+}
